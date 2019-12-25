a652271a06c85f4d1eb6bae9e18be85fcf0c2163
tvikneshwar <tvikneshwar@gmail.com>
2019-12-25 18:48:39 +0530

    First commit

---
+++ x86/Hello-world/hello.c
@@ -0,0 +1,15 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+
+static int __init hello_init(void) {
+  printk(KERN_ALERT "Hello World\n");
+}
+
+static void __exit hello_exit(void) {
+  printk(KERN_ALERT "Goodbye World\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
