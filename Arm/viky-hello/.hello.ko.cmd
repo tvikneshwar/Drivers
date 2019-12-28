fb1d641f704c8f070d4baddb2c38ba81f8d29f7a
tvikneshwar <tvikneshwar@gmail.com>
2019-12-28 05:42:47 +0000

    added hello-world

---
+++ Arm/viky-hello/.hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/pi/Desktop/Driver/exploringBB/extras/kernel/viky-hello/hello.ko := ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/pi/Desktop/Driver/exploringBB/extras/kernel/viky-hello/hello.ko /home/pi/Desktop/Driver/exploringBB/extras/kernel/viky-hello/hello.o /home/pi/Desktop/Driver/exploringBB/extras/kernel/viky-hello/hello.mod.o ;  true
---
+++ Arm/viky-hello/.tmp_versions/hello.mod
@@ -0,0 +1,3 @@
+/home/pi/Desktop/Driver/exploringBB/extras/kernel/viky-hello/hello.ko
+/home/pi/Desktop/Driver/exploringBB/extras/kernel/viky-hello/hello.o
+
